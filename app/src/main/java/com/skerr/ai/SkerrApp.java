c9a3cdfd031885910f7872772c9dd53aab1be302
pierdeoliveira-cell <pier.de.oliveira@gmail.com>
2026-06-23 08:48:09 -0900

    feat: add SkerrApp.java

---
+++ app/src/main/java/com/skerr/ai/SkerrApp.java
@@ -0,0 +1,10 @@
+package com.skerr.ai;
+
+import android.app.Application;
+
+public class SkerrApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
